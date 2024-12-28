ea9cbef7a2abb0d2142f694ed4b6f605dfdc7ba8
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-12-27 23:12:40 -0500

    chore: adds a few docs

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
+# create data frame from csv using pandas
 df = pd.read_csv("nato_phonetic_alphabet.csv")
+
+# create a dictionary from the data frame
 alphabet = {row.letter: row.code for (index, row) in df.iterrows()}
 
+# get input from user
 input_word = input("Enter a word: ")
 
+# create a list of the NATO phonetic alphabet for the input word
 output_list = [alphabet[letter.upper()] for letter in input_word]
 
 print(output_list)
