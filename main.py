2992e1d6677100e24ba79ff425b94776442dc6be
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-12-27 23:14:25 -0500

    refactor: optimization - uppercase user's input, not each letter

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ df = pd.read_csv("nato_phonetic_alphabet.csv")
 alphabet = {row.letter: row.code for (index, row) in df.iterrows()}
 
 # get input from user
-input_word = input("Enter a word: ")
+input_word = input("Enter a word: ").upper()
 
 # create a list of the NATO phonetic alphabet for the input word
-output_list = [alphabet[letter.upper()] for letter in input_word]
+output_list = [alphabet[letter] for letter in input_word]
 
 print(output_list)
